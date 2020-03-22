e8c182dd0c88d31fce88a5ff20260c612d448c8e
eduardomoron151 <eduardomoron151@gmail.com>
2020-03-21 22:41:02 -0400

    Creacion de Contacto y galeria

---
+++ public/js/main2.js
@@ -0,0 +1,98 @@
+$('.carousel').carousel({
+  interval: 3000
+});
+
+const sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: 'bottom'
+});
+
+sr.reveal('#slider', {
+  duration: 2000,
+  origin: 'top',
+  distance: '150px'
+});
+
+sr.reveal('#contact', {
+  duration: 2000,
+  origin: 'top',
+  distance: '150px'
+});
+
+sr.reveal('.aboutUs-title', {
+  duration: 2000,
+  origin: 'top',
+  distance: '150px'
+});
+
+sr.reveal('#aboutUs-obj-resp', {
+  duration: 2000,
+  origin: 'left',
+  distance: '150px'
+});
+
+
+sr.reveal('#aboutUs-mis-vis', {
+  duration: 2000,
+  origin: 'right',
+  distance: '150px'
+});
+
+sr.reveal('#aboutUs-instalaciones', {
+  duration: 2000,
+  origin: 'top',
+  distance: '150px'
+});
+
+sr.reveal('#aboutUs-jumbotron', {
+  duration: 2000,
+  origin: 'top',
+  distance: '150px'
+});
+
+sr.reveal('#ProyectoEducativo', {
+  duration: 2000,
+  origin: 'top',
+  distance: '150px'
+});
+
+sr.reveal('#ProyectoEducativoPropio', {
+  duration: 2000,
+  origin: 'left',
+  distance: '150px'
+});
+
+sr.reveal('#MetodologiaActiva', {
+  duration: 2000,
+  origin: 'right',
+  distance: '150px'
+});
+
+sr.reveal('#OrientacionBilingue', {
+  duration: 2000,
+  origin: 'left',
+  distance: '150px'
+});
+
+sr.reveal('#FamiliasParticipativas', {
+  duration: 2000,
+  origin: 'right',
+  distance: '150px'
+});
+
+sr.reveal('#ProyectoVivo', {
+  duration: 2000,
+  origin: 'left',
+  distance: '150px'
+});
+
+
+
+
+
+
+
+
+
